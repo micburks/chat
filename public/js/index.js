e399c4583b4ff73bb89e81fa612978d34dca6de0
Mickey Burks <brks.mck@gmail.com>
2022-04-03 12:22:55 -0500

    Add jspm import map

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
 /* eslint-env browser */
+import SimplePeer from 'simple-peer';
 import {io} from 'https://cdn.socket.io/4.4.1/socket.io.esm.min.js';
 const socket = io(location.host);
 
@@ -16,13 +17,20 @@ const createButton = document.getElementById('create-chat');
 createButton.addEventListener('click', () => {
   socket.emit('create-chat', socket.id, (chatId) => {
     console.log(chatId);
-    setChat(chatId);
+    createChat(chatId);
   });
   createButton.hidden = true;
 });
 
-function setChat(id) {
-  container.innerHTML = `<div>chat: ${id}</div>`;
+function createChat(id) {
+  peer = createPeer(true);
+  container.innerHTML = Chat(id);
+
+  document.getElementById('send').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const msg = document.getElementById('msg').value;
+    sendMessage(msg);
+  });
 }
 
 chat.addEventListener('click', (e) => {
@@ -34,8 +42,44 @@ chat.addEventListener('click', (e) => {
   });
 });
 
+let peer = null;
+function createPeer(initiator) {
+  peer = new SimplePeer({initiator});
+  peer.on('error', (err) => console.log('error', err));
+  peer.on('signal', (data) => {
+    console.log('SIGNAL', JSON.stringify(data));
+  });
+  peer.on('connect', () => {
+    console.log('CONNECT');
+    peer.send('whatever' + Math.random());
+  });
+  peer.on('data', (data) => {
+    console.log('data: ' + data);
+  });
+  return peer;
+}
+
 function joinChat(id) {
-  container.innerHTML = `
+  peer = createPeer(false);
+  container.innerHTML = Chat(id);
+
+  document.getElementById('send').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const msg = document.getElementById('msg').value;
+    sendMessage(msg);
+  });
+}
+
+function sendMessage(msg) {
+  if (!peer) {
+    console.log('no peers connected');
+    return;
+  }
+  peer.signal(msg);
+}
+
+function Chat(id) {
+  return `
 <div>
   chat: ${id}
   <div id="messages"></div>
@@ -46,9 +90,10 @@ function joinChat(id) {
   </div>
 </div>
 `;
-  document.getElementById('send').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const msg = document.getElementById('msg').value;
-    console.log(msg);
-  });
+}
+
+function ListElement(text) {
+  return `
+<li class="chat-list">${text}</li>
+`;
 }
